82c573bb3b392d0590b27d4a22f21d7cbee1b4e5
Michael Belhassen <mikou12345@gmail.com>
2017-05-22 12:48:05 +0100

    Stupid Coach 2.0

---
+++ app/controllers/coaching_controller_controller.rb
@@ -0,0 +1,34 @@
+class CoachingControllerController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:user_question]
+    @answer = coach_answer_enhanced(@question)
+  end
+
+  private
+
+  def coach_answer(your_message)
+    # TODO: return coach answer to your_message
+    if your_message == "I am going to work right now!"
+      return ""
+    elsif your_message.include? "?"
+      return "Silly question, get dressed and go to work!"
+    else
+      return "I don't care, get dressed and go to work!"
+    end
+  end
+
+  def coach_answer_enhanced(your_message)
+    if your_message.downcase == "i am going to work right now!"
+      return ""
+    elsif your_message == your_message.upcase
+      answer = coach_answer(your_message)
+      return "I can feel your motivation! #{answer}"
+    else
+      return coach_answer(your_message)
+    end
+  end
+
+end
